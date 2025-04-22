a9cd835dce533d925ad5ecbbbf3d6ef38182522c
Bianca Lisle <bianca.vianadeaguiar@mongodb.com>
2025-04-22 15:46:45 +0100

    feat: use machineid library

--- src/telemetry/constants.ts
+++ src/telemetry/constants.ts
@@ -1,11 +1,12 @@
 import pkg from "../../package.json" with { type: "json" };
 import config from "../config.js";
+import { getMachineIdSync } from 'native-machine-id';
 
 /**
  * Machine-specific metadata formatted for telemetry
  */
 export const MACHINE_METADATA = Object.freeze({
-    device_id: "id", // TODO: use @mongodb-js/machine-id
+    device_id: getMachineIdSync(),
     mcp_server_version: pkg.version,
     mcp_server_name: config.mcpServerName,
     platform: process.platform,
