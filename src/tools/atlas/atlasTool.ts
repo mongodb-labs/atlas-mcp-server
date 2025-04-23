8505d918315ff3eff88e30ee11f990cf5e4ef991
Bianca Lisle <bianca.vianadeaguiar@mongodb.com>
2025-04-23 17:46:50 +0100

    fix check

--- src/tools/atlas/atlasTool.ts
+++ src/tools/atlas/atlasTool.ts
@@ -1,6 +1,4 @@
 import { ToolBase, ToolCategory } from "../tool.js";
-import { Session } from "../../session.js";
-import { Telemetry } from "../../telemetry/telemetry.js";
 
 export abstract class AtlasToolBase extends ToolBase {
     protected category: ToolCategory = "atlas";
