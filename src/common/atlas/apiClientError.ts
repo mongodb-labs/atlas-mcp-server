f4613f4262904bad24395c00fe2f97b862775a73
Filipe C Menezes <filipe.menezes@mongodb.com>
2025-04-30 10:38:32 +0100

    fix: improve api error messages

--- src/common/atlas/apiClientError.ts
+++ src/common/atlas/apiClientError.ts
@@ -1,21 +1,41 @@
-export class ApiClientError extends Error {
-    response?: Response;
+import { ApiError } from "./openapi.js";
 
-    constructor(message: string, response: Response | undefined = undefined) {
+export class ApiClientError extends Error {
+    private constructor(
+        message: string,
+        public readonly response?: Response,
+        public readonly body?: ApiError
+    ) {
         super(message);
         this.name = "ApiClientError";
-        this.response = response;
     }
 
     static async fromResponse(
         response: Response,
         message: string = `error calling Atlas API`
     ): Promise<ApiClientError> {
+        let text: string = "";
+        let body: ApiError | undefined = undefined;
         try {
-            const text = await response.text();
-            return new ApiClientError(`${message}: [${response.status} ${response.statusText}] ${text}`, response);
+            body = (await response.json()) as ApiError;
+            text = body.reason || "unknown error";
+            if (body.detail && body.detail.length > 0) {
+                text = `${text}; ${body.detail}`;
+            }
         } catch {
-            return new ApiClientError(`${message}: ${response.status} ${response.statusText}`, response);
+            try {
+                text = await response.text();
+            } catch {
+                text = "";
+            }
         }
+
+        if (text.length > 0) {
+            text = `${message}: [${response.status} ${response.statusText}] ${text.trim()}`;
+        } else {
+            text = `${message}: ${response.status} ${response.statusText}`;
+        }
+
+        return new ApiClientError(text, response, body);
     }
 }
